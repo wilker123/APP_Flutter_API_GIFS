43aae819c1b5aa90c8cabfad206adfd70b250f87
Wilker <josewilkerlucena123@gmail.com>
2021-10-22 22:53:10 -0300

    Adicionando botão de compartilhar gifs e tambem a opção de compartilhar ao pressionar segurando no gif

--- search_gifs/lib/ui/gif_page.dart
+++ search_gifs/lib/ui/gif_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
  class GifPage extends StatelessWidget {
   
@@ -13,6 +14,14 @@ import 'package:flutter/material.dart';
        appBar: AppBar(
          title: Text(_gifData['title']),
         backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: (){
+              Share.share(_gifData['images']['fixed_height']['url']);
+            }
+          )
+        ],
        ),
        backgroundColor: Colors.black,
        body: Center(
--- search_gifs/lib/ui/homePage.dart
+++ search_gifs/lib/ui/homePage.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 import 'gif_page.dart';
 
@@ -112,11 +114,17 @@ class _HomePageState extends State<HomePage> {
       itemBuilder: (context, index){
         if(_search == "" || index < snapshot.data['data'].length){
             return GestureDetector(
-            child: Image.network(snapshot.data['data'][index]['images']['fixed_height']['url'],
-            height: 300.0, fit: BoxFit.cover),
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage, 
+              image: snapshot.data['data'][index]['images']['fixed_height']['url'],
+              height: 300.0, fit: BoxFit.cover
+            ),
             onTap: (){
               Navigator.push(context, MaterialPageRoute(builder: (context) => GifPage(snapshot.data['data'][index])));
             },
+            onLongPress: (){
+              Share.share(snapshot.data['data'][index]['images']['fixed_height']['url']);
+            }
           );
         }else{
           return Container(
